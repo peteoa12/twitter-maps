5962d70ae8ec74b8aabd50d60ee61a2e813b715a
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2016-11-27 20:50:33 -0500

    Remove the now ignored directory config.inc.php

--- config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$twitteruser = "twitterusername";
-$consumerkey = "nExUvyCEwo8BmVKZS5zVKrik6";
-$consumersecret = "6nM8iQuTkfEKCGU14VbpaAjrSDiRWd2kXHajXgQ3snUyaffiDq";
-$accesstoken = "435120030-n60UjPI2jegF7vYinXNDZZpZpZvtDc7bhV2NnEbG";
-$accesstokensecret = "crf4YXCwJgTFrbq9X2vX8oAlbn7iItsY1U49Txlie8ZLS";
